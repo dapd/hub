b8c2e493cd91f0e9d697757d11f9e327936cdaf4
dapd <dapd@cin.ufpe.br>
2016-11-22 20:12:45 -0300

    Update hubParaModulo.py
    Implementação dos métodos presentes nessa classe

--- hubParaModulo.py
+++ hubParaModulo.py
@@ -3,9 +3,13 @@
 ##
 ## @brief      Class for hub para modulo.
 ##
+import time
+import serial
+
 class HubParaModulo(object):
 	
 	status = ""
+	ok='OK\r\n'
 
 	##
 	## @brief      Constructs the object.
@@ -14,8 +18,16 @@ class HubParaModulo(object):
 	## @param      arg   The argument
 	##
 	def __init__(self, arg):
-		super(ubParaModulo, self).__init__()
+		super(HubParaModulo, self).__init__()
 		self.arg = arg
+		ser = serial.Serial(
+		  port='/dev/ttyAMA0',
+		  baudrate=9600,
+		  parity=serial.PARITY_NONE,
+		  stopbits=serial.STOPBITS_ONE,
+		  bytesize=serial.EIGHTBITS,
+		  timeout=1
+		)
 	
 	##
 	## @brief      { function_description }
@@ -24,8 +36,73 @@ class HubParaModulo(object):
 	##
 	## @return     { description_of_the_return_value }
 	##
+	def parear(self,idt,pin):
+		#print('Pressione o botao do bluetooth')
+		time.sleep(5)
+		
+		ser.write('AT+ROLE=1\r\n') #define o modo de operacao do modulo como MASTER
+		x=ser.readline()
+		if(x!=ok):
+			print('Comando AT nao funcionou')
+			return False
+		#print(x)
+		
+		ser.write('AT+CMODE=1\r\n') #Permite a conexao a qualquer endereco
+		x=ser.readline()
+		if(x!=ok):
+			print('Comando AT nao funcionou')
+			return False
+		#print(x)
+		
+		ser.write('AT+PSWD=%d\r\n'%(pin))  #define a senha do modulo mestre, que deve ser a mesma do modulo slave/escravo
+		x=ser.readline()
+		if(x!=ok):
+			print('Comando AT nao funcionou')
+			return False
+		#print(x)
+		
+		ser.write('AT+PAIR=%s,10\r\n'%(idt))  #PAREAR COM O DISPOSITIVO
+		time.sleep(5)
+		x=ser.readline()
+		#print(x)
+		
+		ser.write('AT+LINK=%s\r\n'%(idt))  #CONECTAR AO DISPOSITIVO
+		x=ser.readline()
+		#print(x)
+		
+		ser.write('AT+ROLE=0\r\n') #define o modo de operacao do modulo como SLAVE
+		x=ser.readline()
+		if(x!=ok):
+			print('Comando AT nao funcionou')
+			return False
+		#print(x)
+		
+		ser.write('OK')
+		time.sleep(1)
+		x=ser.readline()
+		if(x!=ok):
+			print('Pareamento falhou')
+			return False
+		
+		return True
+	
 	def conectarModulo(modulo):
-		return False
+		ser.write('AT+ROLE=1\r\n') #define o modo de operacao do modulo como MASTER
+		x=ser.readline()
+		if(x!=ok):
+			print('Comando AT nao funcionou')
+			return False
+		#print(x)
+		
+		ser.write('AT+LINK=%s\r\n'%(modulo))  #CONECTAR AO DISPOSITIVO
+		
+		ser.write('AT+ROLE=0\r\n') #define o modo de operacao do modulo como SLAVE
+		x=ser.readline()
+		if(x!=ok):
+			print('Comando AT nao funcionou')
+			return False
+		#print(x)
+		return True
 
 	##
 	## @brief      { function_description }
@@ -33,7 +110,11 @@ class HubParaModulo(object):
 	## @return     { description_of_the_return_value }
 	##
 	def receberModulo():
-		return "ASD"
+		x=ser.readline()
+		if(x[0]!=48 | x[0]!=49):
+			return (False,x)
+		else:
+			return (True,x)
 
 	##
 	## @brief      { function_description }
@@ -43,4 +124,10 @@ class HubParaModulo(object):
 	## @return     { description_of_the_return_value }
 	##
 	def mandarModulo(mensagem):
-		pass
+		ser.write(mensagem)
+		x=ser.readline()
+		if(x!=ok):
+			print('Comando AT nao funcionou')
+			return False
+		#print(x)
+		return True
