d5a7c2bc77d5887c9f6d5640616cbe7ddb3bd643
Juniorlimaivd <junior.lima.ivd.2@gmail.com>
2016-12-10 16:45:16 -0300

    Update hubParaModulo2.py

--- hubParaModulo2.py
+++ hubParaModulo2.py
@@ -42,7 +42,7 @@ class adaptadorBluetooth:
 				ret = self.serialConnection.readline()
 				ret = ret.decode().strip('\r\n')
 				print (ret," OK")
-				#self.serialConnection.setBaudrate(9600)
+				self.serialConnection.setBaudrate(9600)
 				
 			self.AT=False
 
@@ -69,7 +69,7 @@ class adaptadorBluetooth:
 			ret = ret.decode().strip('\r\n')
 			print (ret," UART")
 			
-			self.serialConnection.setBaudrate(38400)
+			#self.serialConnection.setBaudrate(38400)
 		self.AT=True
 
 	def sendToSerial(self, message, cmd, ok):
